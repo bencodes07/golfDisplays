da98a4c5b65a17a68dff323269985138e2b647ad
Ben Böckmann <boeckmannben@gmail.com>
2024-08-10 23:42:42 +0200

    Bug fix

--- CockpitGear/CockpitGear.ino
+++ CockpitGear/CockpitGear.ino
@@ -99,7 +99,7 @@ void loop()
             u8g2.clearBuffer();
             u8g2.setFont(u8g2_font_logisoso26_tr);
             u8g2.setCursor(-2, 28);
-            u8g2.print();
+            u8g2.print("");
             u8g2.sendBuffer();
 
             delay(200);
